1249aa0d6493368bc1b3d2892fa6e9e3dbc30f31
Firm-Soft <evan.w.mclaughlin@gmail.com>
2021-03-01 11:28:26 -0500

    Update to in-progress

--- src/data/doc-it/R19Roadmap.ts
+++ src/data/doc-it/R19Roadmap.ts
@@ -112,7 +112,7 @@ export const roadmap: RoadmapBucket[] = [
                 description: 'Designer templates / examples for Login and Registration workflows',
                 year: 2021,
                 quarter: 'Q1',
-                status: 'backlog',
+                status: 'in-progress',
                 author: 'PX Blue',
             },
             {
