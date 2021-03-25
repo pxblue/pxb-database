96d94535309cd8780edfd044bcc5f0f88c40be0d
joebochill <boyle.p.joseph@gmail.com>
2021-03-25 11:22:00 -0400

    Split out roadmaps into separate files, update script to rebuild all

---
+++ src/data/doc-it/R20Roadmap.ts
@@ -0,0 +1,148 @@
+export type FrameworkFilter = 'all' | 'angular' | 'react' | 'ionic' | 'react-native';
+export type ItemTypeFilter = 'all' | 'design' | 'development';
+export type Status = 'backlog' | 'in-progress' | 'pre-release' | 'deferred' | 'finished';
+export type Quarter = 'Q1' | 'Q2' | 'Q3' | 'Q4';
+
+const CURRENT_YEAR = 2021;
+const CURRENT_QUARTER: Quarter = 'Q2';
+
+export type RoadmapItem = {
+    name: string;
+    description: string;
+    year: string | number;
+    quarter: Quarter;
+    status: Status;
+    author?: string;
+    type?: ItemTypeFilter;
+    framework?: FrameworkFilter[];
+};
+type RoadmapBucket = {
+    name: string;
+    description: string;
+    type: ItemTypeFilter;
+    framework?: FrameworkFilter[];
+    items: RoadmapItem[];
+};
+
+export const roadmap: RoadmapBucket[] = [
+    {
+        name: 'Figma Component Library',
+        description: 'Updates to the components in the designer stickersheet',
+        type: 'design',
+        items: [
+            {
+                name: 'Dark Theme for Components',
+                description: 'Dark theme variations for all of our design components',
+                year: CURRENT_YEAR,
+                quarter: CURRENT_QUARTER,
+                status: 'backlog',
+                author: 'PX Blue',
+            },
+        ],
+    },
+    {
+        name: 'Figma Design Patterns',
+        description: 'Adding design pattern examples to Figma',
+        type: 'design',
+        items: [ ],
+    },
+    {
+        name: 'Angular Component Library',
+        description: 'Reusable UI elements from @pxblue/angular-components',
+        type: 'development',
+        framework: ['angular', 'ionic'],
+        items: [
+            {
+                name: 'Collapsible App Bar',
+                description: 'Add an app bar component that shrinks and condenses its content on scroll',
+                year: CURRENT_YEAR,
+                quarter: CURRENT_QUARTER,
+                status: 'backlog',
+                author: 'PX Blue',
+            },
+        ],
+    },
+    {
+        name: 'React Component Library',
+        description: 'Reusable UI elements from @pxblue/react-components',
+        type: 'development',
+        framework: ['react'],
+        items: [
+            {
+                name: 'Collapsible App Bar',
+                description: 'Add an app bar component that shrinks and condenses its content on scroll',
+                year: CURRENT_YEAR,
+                quarter: CURRENT_QUARTER,
+                status: 'backlog',
+                author: 'PX Blue',
+            },
+        ],
+    },
+    {
+        name: 'React Native Component Library',
+        description: 'Reusable UI elements from @pxblue/react-native-components',
+        type: 'development',
+        framework: ['react-native'],
+        items: [
+            {
+                name: 'Collapsible App Bar',
+                description: 'Add an app bar component that shrinks and condenses its content on scroll',
+                year: CURRENT_YEAR,
+                quarter: CURRENT_QUARTER,
+                status: 'backlog',
+                author: 'PX Blue',
+            },
+            {
+                name: 'Expandable Bottomsheet',
+                description: 'Add an bottomsheet that can be expanded when swiped up',
+                year: CURRENT_YEAR,
+                quarter: CURRENT_QUARTER,
+                status: 'backlog',
+                author: 'PX Blue',
+            },
+        ],
+    },
+    {
+        name: 'pxblue.github.io',
+        description: 'The PX Blue documentation site',
+        type: 'design',
+        items: [ ],
+    },
+    {
+        name: 'Code Examples',
+        description: 'Basic code snippets showing how to implement various design patterns',
+        type: 'development',
+        framework: ['all'],
+        items: [ ],
+    },
+    {
+        name: 'Reusable Workflows',
+        description: 'Components for implementing common workflows across applications',
+        type: 'development',
+        framework: ['all'],
+        items: [ ],
+    },
+    {
+        name: 'Themes',
+        description: 'Styles and colors for applications',
+        type: 'development',
+        framework: ['all'],
+        items: [ ],
+    },
+    {
+        name: 'Utilities',
+        description: 'Assorted tools for helping you work faster',
+        type: 'development',
+        framework: ['all'],
+        items: [
+            {
+                name: 'PX Blue CLI - Ionic',
+                description: 'Ionic project templates for the CLI',
+                year: CURRENT_YEAR,
+                quarter: CURRENT_QUARTER,
+                status: 'backlog',
+                author: 'PX Blue',
+            },
+        ],
+    },
+];
